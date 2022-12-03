17dbf0c90924015887144e2334b9cca3262de359
Theophile Tagne <tagnepierre9@gmail.com>
2022-12-03 04:53:00 +0100

    add navbar

---
+++ js/nav_link_coloriser.js
@@ -0,0 +1,23 @@
+/**
+ * Loop through links to add or remove 'active' class
+ * @see https://alvarotrigo.com/blog/sticky-navbar/
+ */
+const addOrRemoveActiveClass = () => {
+
+    let navbarLinks = document.querySelectorAll('.nav-links a')
+
+    window.addEventListener('scroll', event => {
+        scrollPos = window.scrollY
+
+        navbarLinks.forEach((link) => {
+            const section = document.querySelector(link.hash);
+             if (scrollPos + 100 > section.offsetTop && scrollPos + 100 < section.offsetTop + section.offsetHeight) {
+                link.classList.add('active')
+             } else {
+                link.classList.remove('active')
+             }
+        })
+    })
+}
+
+addOrRemoveActiveClass()
